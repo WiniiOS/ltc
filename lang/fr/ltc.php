6e845acc7451c1f885575f4d9235fe39731bb278
YufreeBay <55185570+YufreeBay@users.noreply.github.com>
2023-01-27 15:50:52 +0100

    Update ltc.php

---
+++ lang/fr/ltc.php
@@ -0,0 +1,62 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | LTC GROUP WEB SITE Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during Showing for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'Home' => 'Accueil',
+    'About' => 'A propos',
+    'Services' => 'Services',
+    'Portfolio' => 'Réalisations',
+    'Team' => 'Equipe',
+    'Contact' => 'Contact',
+    'Language' => 'Langue',
+    'En' => 'Anglais',
+    'Fr' => 'Français',
+    'About-us' => 'Qui sommes nous',
+    'Termsofsevice' => "Conditions d'utilisation",
+    'GraphicDesign' => 'Design Graphique',
+    // Slides
+    'Slide1Title' => 'Bienvenue chez LTC Group En partenariat avec UBA',
+    'Slide1Description' => 'L’entreprise LTC Group SARL est une entreprise exerçant dans divers domaines des nouvelles technologies entre autres le marketing digital, le développement d’application mobile et web, la communication, la publicité sur internet, la prestation de services, bâtiments et travaux publics, et le commerce général.',
+    'SlideButton' => 'En savoir plus',
+    'Slide2Title' => "L'E-Market à votre porté",
+    'Slide2Description' => "LTC Group SARL est une entreprise mets à votre disposition  divers produits électroniques via sa plateforme d'e-commerce et ses réseaux sociaux.",
+    'Slide3Title' => "GO-LIVRAISON / LTC GROUP HOST & Marketing Digital",
+    'Slide3Description' => 'Notre service de livraison à votre disposition,La publicité sur internet (Facebook, Instagram, YouTube...) ✓ La communication digitale : gestion des pages et des contenus digitaux ✓ La création des logos ✓ La création des affiches ✓ Graphisme.',
+    // About section translate
+    'About-us' => 'Qui sommes-nous ?',
+    'AboutDescription' => 'La société LTC Group SARL est une société opérant dans divers domaines des nouvelles technologies, notamment le marketing numérique, le développement d'applications mobiles et web, la communication, la publicité sur Internet, la prestation de services, les bâtiments et travaux publics, et le commerce général.',
+    'AboutFirstItemTitle' => 'LTC GROUP',
+    'AboutSecondItemTitle' => 'LTC GROUP COMMUNICATION',
+    'AboutSecondDesc' => 'Cette branche est dédiée au marketing digital entre autres : ✓ Publicité sur Internet (Facebook, Instagram, YouTube...) ✓ Communication digitale : gestion des pages et contenus digitaux ✓ Création de logos ✓ Création d'affiches ✓ Conception graphique.',
+    'AboutFirstDesc' => 'En partenariat avec UBA , nous mettons à votre disposition ✓ des cartes visa prépayées UBA ✓ des recharges de cartes visa prépayées UBA ✓ des formations.',
+    // Statistics
+    'Stat1' => 'Satisfied customers',
+    'Stat2' => 'Achievements',
+    'Stat3' => 'Support Hours',
+    'Stat4' => 'Members',
+    // Services
+    'Service1' => 'LTC GROUP',
+    'Service2' => 'LTC GROUP COMMUNICATION',
+    'Service3' => 'LTC GROUP HOST',
+    'Service4' => 'E-MARKET',
+    'Service5' => 'GO LIVRAISON',
+    'Service6' => 'CUSTOMER SERVICE',
+    'Service1Desc' => 'About Us',
+    'Service2Desc' => 'About Us',
+    'Service3Desc' => 'About Us',
+    'Service4Desc' => 'About Us',
+    'Service5Desc' => 'About Us',
+    'Service6Desc' => 'About Us',
+
+];
